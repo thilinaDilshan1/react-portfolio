7d88e67d8dc8118d33301bbecba6f5b293720bb5
Thilina Dilshan <tilinadilshan1999@gmail.com>
2024-02-25 22:01:54 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
-import './App.css';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Skills from './Pages/Skills/Skills';
+import Projects from './Pages/Projects/Projects';
+import Contact from './Pages/Contact/Contact';
 
 function App() {
   return (
     <div>
       <Home/>
+      <Router>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Router>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Navbar.css';
-import Home from '../../Pages/Home/Home';
-import About from '../../Pages/About/About';
-import Skills from '../../Pages/Skills/Skills';
-import Projects from '../../Pages/Projects/Projects';
-import Contact from '../../Pages/Contact/Contact';
 
 export default function Navbar() {
   return (
@@ -14,7 +9,6 @@ export default function Navbar() {
             <div className="navbarLeft">
               <h2>Portfolio</h2>
             </div>
-            <Router>
               <div className="navbarRight">
                 <div className="navbarItems">
                   <Link to="/" className="navbarItem">Home</Link>
@@ -25,12 +19,6 @@ export default function Navbar() {
                 </div>
               </div>
 
-              <Route path="/" exact render={() => <Home />} />
-              <Route path="/about" render={() => <About />} />
-              <Route path="/skills" render={() => <Skills />} />
-              <Route path="/projects" render={() => <Projects />} />
-              <Route path="/contact" render={() => <Contact />} />
-            </Router>
         </div>
     </div>
   )
