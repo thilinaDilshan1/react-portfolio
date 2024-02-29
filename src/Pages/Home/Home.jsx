a49f2fa657a3c8d69941d979ef7cd62f838cc24e
Thilina Dilshan <tilinadilshan1999@gmail.com>
2024-02-29 12:59:24 +0530

    create home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,47 @@
 import React from 'react'
 import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
+import homeImg from '../../Assests/home.png'
 
 export default function Home(currentPage) {
   return (
     <div>
       <Navbar currentPage={currentPage} />
-      <h1>Home</h1>
+      <div className="homeContainer">
+        <div class="home-content">
+          <h3 class="me">Hello,It's Me</h3>
+          <h1>Thilina Dilshan</h1>
+          <h3>
+            And I'm a <span class="multi-text"> </span>
+          </h3>
+          <p>
+            I'm a Graphic Designer, Programmer, Web developer and a UI/UX
+            Designer. I'm a Undergraduate Student at Eastern University Sri
+            Lanka.
+          </p>
+          <div class="sociel-media">
+            <a href="www.facebook.com">
+              <i class="bx bxl-facebook"></i>
+            </a>
+            <a href="www.instagram.com">
+              <i class="bx bxl-instagram-alt"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/thilina-dilshan-b4bb8920a/">
+              <i class="bx bxl-linkedin"></i>
+            </a>
+            <a href="https://twitter.com/">
+              <i class="bx bxl-twitter"></i>
+            </a>
+          </div>
+          <a href="https://twitter.com/" class="btn">
+            Download CV
+          </a>
+        </div>
+
+        <div class="home-img">
+          <img src={homeImg} alt="my img" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
